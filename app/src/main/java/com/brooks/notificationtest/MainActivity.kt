04357c3e9eaa87366e3b3cfd92cf903ea12474f8
brooks <myt2000@126.com>
2022-11-14 15:23:30 +0800

    添加通知效果，点击通知跳转到页面，点击通知后，通知在状态栏取消通知（消失）

--- app/src/main/java/com/brooks/notificationtest/MainActivity.kt
+++ app/src/main/java/com/brooks/notificationtest/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.brooks.notificationtest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(1)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        sendNotice.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            val pi = PendingIntent.getActivity(this, 0 , intent, 0)
+
+            val notification = NotificationCompat.Builder(this, "normal")
+                .setContentTitle("This is content files")
+                .setContentText("This is content text")
+                .setSmallIcon(R.drawable.small_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+//                .setStyle()
+                .build()
+            manager.notify(1, notification)
+        }
     }
 }
+
+
