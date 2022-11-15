48342b724d0c515168f96396b99c6a102a1aa3f7
brooks <myt2000@126.com>
2022-11-15 17:42:57 +0800

    新建一个通知渠道2，设置为通知消息的最高等级

--- app/src/main/java/com/brooks/notificationtest/MainActivity.kt
+++ app/src/main/java/com/brooks/notificationtest/MainActivity.kt
@@ -17,23 +17,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        manager.cancel(1)
+//        manager.cancel(1)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
-            manager.createNotificationChannel(channel)
+//            val channel = NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
+//            manager.createNotificationChannel(channel)
+            val channel2 = NotificationChannel("important", "Important", NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel2)
         }
         sendNotice.setOnClickListener {
             val intent = Intent(this, NotificationActivity::class.java)
             val pi = PendingIntent.getActivity(this, 0 , intent, 0)
 
-            val notification = NotificationCompat.Builder(this, "normal")
+//            val notification = NotificationCompat.Builder(this, "normal")
+            val notification = NotificationCompat.Builder(this, "important")
                 .setContentTitle("This is content files")
-                .setContentText("This is content text")
+                .setContentText("Learn how to build notifications, send and sync data, and use actions. Get The official Android IDE and developer tools to build apps for Android.")
                 .setSmallIcon(R.drawable.small_icon)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
                 .setContentIntent(pi)
+//                .setStyle(NotificationCompat.BigTextStyle().bigText("Learn how to build notifications, send and sync data, and use actions. Get The official Android IDE and developer tools to build apps for Android."))
+//                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(resources, R.drawable.big_image)))
                 .setAutoCancel(true)
-//                .setStyle()
                 .build()
             manager.notify(1, notification)
         }
